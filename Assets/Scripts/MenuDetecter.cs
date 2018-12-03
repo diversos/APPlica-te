0a270b533c2f309a74e96e6223580922353ae307
Bruno Grifo <leitaogrifo@gmail.com>
2018-12-03 03:25:06 +0000

    menu area detecting

---
+++ Assets/Scripts/MenuDetecter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDetecter : MonoBehaviour {
+    private bool walking = false;
+
+    private bool showMenu;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+        if (walking) {
+            transform.position = transform.position + Camera.main.transform.forward * 4f * Time.deltaTime;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name.Contains("MenuArea"))
+            {
+                walking = true;
+            }
+            else
+            {
+                walking = false;
+            }
+        }
+    }   
+}
